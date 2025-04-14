878030a3f10de5b6201aff06313af61ad0a296bc
MWM02 <wijem004@gmail.com>
2025-04-14 15:00:11 +0100

    imports activity dummy data and iterates through activity list

--- src/components/Itinerary.jsx
+++ src/components/Itinerary.jsx
@@ -1,9 +1,21 @@
-import {Activity} from "./Activity.jsx";
-
+import { Activity } from "./Activity.jsx";
+import { ListGroup } from "react-bootstrap";
+import { useState } from "react";
+import activities from "../dummy_data/activities.json";
 
 export const Itinerary = () => {
-
+  const [itineraryActivities, setItineraryActivities] = useState(
+    activities.filter((activity) => activity.itinerary_id === 1)
+  );
+  // fetch activity list by itinerary id and iterate through the list and render each Activity component
   return (
-      <Activity/>
+    <ListGroup>
+      {itineraryActivities.map((itineraryActivity) => (
+        <Activity
+          key={itineraryActivity.activity_id}
+          itineraryActivity={itineraryActivity}
+        />
+      ))}
+    </ListGroup>
   );
 };
