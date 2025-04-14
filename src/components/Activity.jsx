cf0f5d80ff29088a6a7d935aba83fc704f988276
MWM02 <wijem004@gmail.com>
2025-04-14 14:58:03 +0100

    adds Modal to activity

--- src/components/Activity.jsx
+++ src/components/Activity.jsx
@@ -1,3 +1,51 @@
-export const Activity = () => {
-  return <div>Activity</div>;
+import Button from "react-bootstrap/Button";
+import Modal from "react-bootstrap/Modal";
+import { useState } from "react";
+import { ListGroup } from "react-bootstrap";
+
+const MyVerticallyCenteredModal = (props) => {
+  return (
+    <Modal
+      {...props}
+      size="lg"
+      aria-labelledby="contained-modal-title-vcenter"
+      centered
+    >
+      <Modal.Header closeButton>
+        <Modal.Title id="contained-modal-title-vcenter">
+          {props.itineraryactivity.title}
+        </Modal.Title>
+      </Modal.Header>
+      <Modal.Body>
+        <h4>Centered Modal</h4>
+        <p>
+          Cras mattis consectetur purus sit amet fermentum. Cras justo odio,
+          dapibus ac facilisis in, egestas eget quam. Morbi leo risus, porta ac
+          consectetur ac, vestibulum at eros.
+        </p>
+      </Modal.Body>
+      <Modal.Footer>
+        <Button onClick={props.onHide}>Close</Button>
+      </Modal.Footer>
+    </Modal>
+  );
+};
+
+export const Activity = ({ itineraryActivity }) => {
+  const [modalShow, setModalShow] = useState(false);
+
+  return (
+    <ListGroup.Item>
+      <p>{itineraryActivity.title}</p>
+      <Button variant="primary" onClick={() => setModalShow(true)}>
+        Show more
+      </Button>
+
+      <MyVerticallyCenteredModal
+        itineraryactivity={itineraryActivity}
+        show={modalShow}
+        onHide={() => setModalShow(false)}
+      />
+    </ListGroup.Item>
+  );
 };
