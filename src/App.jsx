a4a73f654432ed6c7a1e3760d8c0efbe3d2793aa
MWM02 <wijem004@gmail.com>
2025-04-14 09:52:56 +0100

    adds routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,22 @@
-import "./App.css";
 import { UserProvider } from "./context/User";
+import { Routes, Route } from "react-router-dom";
+import { HomePage } from "./components/HomePage";
+import { CountryPage } from "./components/CountryPage";
+import { UserPage } from "./components/UserPage";
+import { UserSettingsPage } from "./components/UserSettingsPage";
+import "./App.css";
 
 function App() {
-  return <UserProvider></UserProvider>;
+  return (
+    <UserProvider>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/:country" element={<CountryPage />} />
+        <Route path="/:username" element={<UserPage />} />
+        <Route path="/:username/settings" element={<UserSettingsPage />} />
+      </Routes>
+    </UserProvider>
+  );
 }
 
 export default App;
