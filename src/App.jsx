ad7b45adc4ea315ba3559ee1641c9873c4f01d60
MWM02 <wijem004@gmail.com>
2025-04-14 10:04:55 +0100

    adds NavBar component

--- src/App.jsx
+++ src/App.jsx
@@ -4,11 +4,13 @@ import { HomePage } from "./components/HomePage";
 import { CountryPage } from "./components/CountryPage";
 import { UserPage } from "./components/UserPage";
 import { UserSettingsPage } from "./components/UserSettingsPage";
+import { NavBar } from "./components/NavBar";
 import "./App.css";
 
 function App() {
   return (
     <UserProvider>
+      <NavBar />
       <Routes>
         <Route path="/" element={<HomePage />} />
         <Route path="/:country" element={<CountryPage />} />
