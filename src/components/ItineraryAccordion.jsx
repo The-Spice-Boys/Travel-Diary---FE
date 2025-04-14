71d69bcb55306444e29419b6516b32a87d3088db
MWM02 <wijem004@gmail.com>
2025-04-14 15:01:06 +0100

    removes second item from accordion

--- src/components/ItineraryAccordion.jsx
+++ src/components/ItineraryAccordion.jsx
@@ -1,27 +1,15 @@
-import Accordion from 'react-bootstrap/Accordion';
-import {Itinerary} from "./Itinerary.jsx";
+import Accordion from "react-bootstrap/Accordion";
+import { Itinerary } from "./Itinerary.jsx";
 
 export const ItineraryAccordion = () => {
   return (
-      <Accordion defaultActiveKey="0">
-    <Accordion.Item eventKey="0">
-      <Accordion.Header>Tokyo Adventure</Accordion.Header>
-      <Accordion.Body>
-        <Itinerary />
-      </Accordion.Body>
-    </Accordion.Item>
-    <Accordion.Item eventKey="1">
-      <Accordion.Header>Accordion Item #2</Accordion.Header>
-      <Accordion.Body>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do
-        eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad
-        minim veniam, quis nostrud exercitation ullamco laboris nisi ut
-        aliquip ex ea commodo consequat. Duis aute irure dolor in
-        reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla
-        pariatur. Excepteur sint occaecat cupidatat non proident, sunt in
-        culpa qui officia deserunt mollit anim id est laborum.
-      </Accordion.Body>
-    </Accordion.Item>
-  </Accordion>
-  )
+    <Accordion defaultActiveKey="0">
+      <Accordion.Item eventKey="0">
+        <Accordion.Header>Tokyo Adventure</Accordion.Header>
+        <Accordion.Body>
+          <Itinerary />
+        </Accordion.Body>
+      </Accordion.Item>
+    </Accordion>
+  );
 };
