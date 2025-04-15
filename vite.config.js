a2cbe6111f9b452b3f4cf04562098ec3301e94f1
jabc8 <jt.024@outlook.com>
2025-04-15 08:56:35 +0100

    add react testing configs

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  test: {
+    environment: 'jsdom',
+    globals: true, // for using `describe`, `it`, `expect`, etc. without importing
+    setupFiles: './src/setupTests.js',
+  },
 })
