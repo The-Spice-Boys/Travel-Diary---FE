6860a8c2c2df58c118dba280de4f7ae8328b9382
Liam Appleton <liamappleton97@outlook.com>
2025-04-16 11:33:19 +0100

    changing class to className

--- src/components/UserPage.jsx
+++ src/components/UserPage.jsx
@@ -1,29 +1,35 @@
-import { useParams } from "react-router-dom";
-import { getItinerariesByUserId, getUserBioByUsername, getUserByUsername } from "../api.js";
-import {ItineraryAccordion} from "./ItineraryAccordion.jsx";
+import { useParams } from 'react-router-dom';
+import {
+  getItinerariesByUserId,
+  getUserBioByUsername,
+  getUserByUsername,
+} from '../api.js';
+import { ItineraryAccordion } from './ItineraryAccordion.jsx';
 
 export const UserPage = () => {
-  const {username} = useParams();
-  const {user_id} = getUserByUsername(username);
+  const { username } = useParams();
+  const { user_id } = getUserByUsername(username);
   const itineraries = getItinerariesByUserId(user_id);
 
-  const {bio, profile_pic_url} = getUserBioByUsername(username)[0]
+  const { bio, profile_pic_url } = getUserBioByUsername(username)[0];
 
   return (
     <>
-    <div class="container-fluid mt-5 mb-2">
-      <h2>Profile</h2>
-      <figure class="figure">
-        <img src={profile_pic_url} class="figure-img img-fluid rounded" alt={username}/>
-      </figure>
-      <p>{bio}</p>
-      
-    </div>
-    <div class="container-fluid text-left">
-      <h2>My itineraries</h2>
-    </div>
-      <ItineraryAccordion itineraries={itineraries}/>
-    
+      <div className="container-fluid mt-5 mb-2">
+        <h2>Profile</h2>
+        <figure className="figure">
+          <img
+            src={profile_pic_url}
+            className="figure-img img-fluid rounded"
+            alt={username}
+          />
+        </figure>
+        <p>{bio}</p>
+      </div>
+      <div className="container-fluid text-left">
+        <h2>My itineraries</h2>
+      </div>
+      <ItineraryAccordion itineraries={itineraries} />
     </>
   );
 };
