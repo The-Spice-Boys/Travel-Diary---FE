b2ff562e99752994033f8e3e1bbab466ed2da948
MWM02 <wijem004@gmail.com>
2025-04-16 12:12:30 +0100

    adds link to login page and home page

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -2,35 +2,32 @@ import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
 import NavDropdown from "react-bootstrap/NavDropdown";
+import { Link } from "react-router-dom";
+import Button from "react-bootstrap/Button";
+import { useContext } from "react";
+import { UserContext } from "../context/User";
 
 export const NavBar = () => {
+  const { loggedInUser } = useContext(UserContext);
+
   return (
     <Navbar collapseOnSelect expand="lg" className="bg-body-tertiary">
       <Container>
-        <Navbar.Brand href="#home">Travel Diary</Navbar.Brand>
-        <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-        <Navbar.Collapse id="responsive-navbar-nav">
-          <Nav className="me-auto">
-            <Nav.Link href="#features">Log Out</Nav.Link>
-            <Nav.Link href="#pricing">Profile</Nav.Link>
-            <Nav.Link href="#deets">Favourites</Nav.Link>
-            <NavDropdown title="Settings" id="collapsible-nav-dropdown">
-              <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.2">
-                Another action
-              </NavDropdown.Item>
-              <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-              <NavDropdown.Divider />
-              <NavDropdown.Item href="#action/3.4">
-                Separated link
-              </NavDropdown.Item>
-            </NavDropdown>
-          </Nav>
-          <Nav>
-            <Nav.Link eventKey={2} href="#memes">
-              Theme
-            </Nav.Link>
-          </Nav>
+        <div className="d-flex w-100 justify-content-between align-items-center">
+          <Navbar.Brand as={Link} to="/">
+            Travel Diary
+          </Navbar.Brand>
+          <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+        </div>
+        <Navbar.Collapse
+          id="responsive-navbar-nav"
+          className="justify-content-end"
+        >
+          {loggedInUser === "" && (
+            <Button as={Link} to="/login">
+              Login
+            </Button>
+          )}
         </Navbar.Collapse>
       </Container>
     </Navbar>
