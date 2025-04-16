efd8bf0a5711da280d2105bac12357834530bf45
MWM02 <wijem004@gmail.com>
2025-04-16 15:38:58 +0100

    adds inline styling

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,14 +1,11 @@
 import Container from "react-bootstrap/Container";
-import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
 import { Link } from "react-router-dom";
 import Button from "react-bootstrap/Button";
 import { useContext } from "react";
 import { UserContext } from "../context/User";
 import { FaRegUser } from "react-icons/fa";
 import { IoSettingsOutline } from "react-icons/io5";
-import { IoLogOutOutline } from "react-icons/io5";
 
 export const NavBar = () => {
   const { loggedInUser, setLoggedInUser } = useContext(UserContext);
@@ -27,19 +24,24 @@ export const NavBar = () => {
         >
           {loggedInUser === "" ? (
             <div className="d-flex flex-lg-row flex-column align-items-lg-center align-items-end justify-content-lg-end gap-3 w-100 mt-lg-0 mt-3">
-              <Button as={Link} to="/login">
-                Login
+              <Button style={{ maxWidth: "200px" }} as={Link} to="/login">
+                Log in
               </Button>
             </div>
           ) : (
             <div className="d-flex flex-lg-row flex-column align-items-lg-center align-items-end justify-content-lg-end gap-3 w-100 mt-lg-0 mt-3">
-              <Button onClick={() => setLoggedInUser("")}>Log out</Button>
               <Link to={`/users/${loggedInUser}`}>
                 <FaRegUser size={24} />
               </Link>
               <Link to={`/users/${loggedInUser}/settings`}>
                 <IoSettingsOutline size={24} />
               </Link>
+              <Button
+                style={{ maxWidth: "200px" }}
+                onClick={() => setLoggedInUser("")}
+              >
+                Log out
+              </Button>
             </div>
           )}
         </Navbar.Collapse>
