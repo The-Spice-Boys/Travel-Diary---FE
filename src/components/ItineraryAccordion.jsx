c71b5c442575af95d3a4e0e1d92eb8dfe387d324
jabc8 <jt.024@outlook.com>
2025-04-17 15:23:59 +0100

    begin implementing api function calls in jsx components

--- src/components/ItineraryAccordion.jsx
+++ src/components/ItineraryAccordion.jsx
@@ -6,26 +6,27 @@ import { useContext, useEffect, useState } from "react";
 import { useParams } from "react-router-dom";
 import { UserContext } from "../context/User.jsx";
 import { FaHeart } from "react-icons/fa";
-import favourites from "../dummy_data/favourites.json";
+import { getFavouritesByUserId, getUserByUsername } from "../api.js";
 
-const Favourite = ({ id }) => {
+const Favourite = ({ itineraryId }) => {
+   const { loggedInUser } = useContext(UserContext);
+   const { user_id } = getUserByUsername(loggedInUser);
+   const favourites = getFavouritesByUserId(user_id);
 
-   // --- MOCK API FUNCTION & TEMP STYLING LOGIC ---
-  const handleFavourite = () => setIsFavourited(!isFavourited);
-  const returnColour = () => isFavourited ? "btn-danger" : "btn-secondary"
-  
-  const [isFavourited, setIsFavourited] = useState(
-     favourites.some(
-        ({ itinerary_id, user_id }) => itinerary_id === id && user_id === 1
-      )
-   );
-   const [colour, setColour] = useState(returnColour());
+   const handleFavourite = () => setIsFavourited(!isFavourited);
+   const returnColour = () => (isFavourited ? "btn-danger" : "btn-secondary");
+
+   const [isFavourited, setIsFavourited] = useState(
+       favourites.some((fave) => {
+           return fave.itinerary_id === itineraryId;
+        })
+    );
+    const [colour, setColour] = useState(returnColour());
 
    useEffect(() => {
       const newColour = isFavourited ? "btn-danger" : "btn-secondary";
       setColour(newColour);
-   }, [isFavourited])
-   // --- MOCK API CALLS & TEMP STYLING LOGIC ---
+   }, [isFavourited]);
 
    return (
       <Button className={colour} onClick={handleFavourite}>
@@ -39,39 +40,43 @@ export const ItineraryAccordion = ({ itineraries }) => {
    const { username } = useParams();
    const editenabled = loggedInUser === username;
 
-   console.log(itineraries)
+   console.log(itineraries);
 
    const accordionItems = itineraries.map(({ itinerary_id, title }) => {
       return (
-          <div key={itinerary_id} className="mb-3 border border-2 border-dark rounded p-3 flex-grow-1 mx-2" id="accordion-item">
-              <Accordion.Item eventKey={itinerary_id}>
-                  <div className="d-flex">
-                      <Accordion.Header className="flex-fill">
-                          {title}
-                      </Accordion.Header>
-                      {editenabled ? (
-                          <MenuPopover icon="dots" className="p-2 ms-2" />
-                      ) : (
-                          <Favourite id={itinerary_id} />
-                      )}
-                  </div>
-                  <Accordion.Body>
-                      <Itinerary
-                          key = {itinerary_id}
-                          itineraryId={itinerary_id}
-                          editenabled={editenabled.toString()}
-                      />
-                  </Accordion.Body>
-              </Accordion.Item>
-          </div>
+         <div
+            key={itinerary_id}
+            className="mb-3 border border-2 border-dark rounded p-3 flex-grow-1 mx-2"
+            id="accordion-item"
+         >
+            <Accordion.Item eventKey={itinerary_id}>
+               <div className="d-flex">
+                  <Accordion.Header className="flex-fill">
+                     {title}
+                  </Accordion.Header>
+                  {editenabled ? (
+                     <MenuPopover icon="dots" className="p-2 ms-2" />
+                  ) : (
+                     <Favourite itineraryId={itinerary_id} />
+                  )}
+               </div>
+               <Accordion.Body>
+                  <Itinerary
+                     key={itinerary_id}
+                     itineraryId={itinerary_id}
+                     editenabled={editenabled.toString()}
+                  />
+               </Accordion.Body>
+            </Accordion.Item>
+         </div>
       );
    });
 
    return (
-       <div className="d-flex  my-5 border border-2 border-dark rounded py-5 w-100">
-               <Accordion className="d-flex flex-row flex-wrap gap-3">
-                   {accordionItems}
-               </Accordion>
-       </div>
-   )
+      <div className="d-flex  my-5 border border-2 border-dark rounded py-5 w-100">
+         <Accordion className="d-flex flex-row flex-wrap gap-3">
+            {accordionItems}
+         </Accordion>
+      </div>
+   );
 };
