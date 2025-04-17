f29807cb69d8da78f4bd2d4cc8c783dee6b66758
msharifabdalla2 <msharifabdalla1@yahoo.com>
2025-04-17 11:57:06 +0100

    Finalised mock api GET calls

--- src/api.js
+++ src/api.js
@@ -3,6 +3,8 @@ import activities from "./dummy_data/activities.json";
 import users from "./dummy_data/users.json";
 import photos from "./dummy_data/photo.json";
 import notes from "./dummy_data/notes.json";
+import countries from "./dummy_data/countries.json";
+
 
 export const getUserByUsername = (requestUsername) => {
    return users.find(({ username }) => username === requestUsername);
@@ -27,3 +29,44 @@ export const getNotesByActivityId = (requestId) => {
 export const getUserBioByUsername = (requestUsername) => {
    return users.filter(({username , bio}) => username === requestUsername ? bio : null);
 }
+
+export const getCountries = () => {
+   return countries;
+}
+
+export const getCountryByName = (countryName) => {
+   return countries.find((country) => country.countryName.toLowerCase() === countryName.toLowerCase());
+}
+
+export const getItinerariesByCountry = (countryName) => {
+   const country = countries.find((country) => country.countryName.toLowerCase() === countryName.toLowerCase());
+
+   return country ? country.itineraries : [];
+}
+
+export const getFavouritesByUsername = (username) => {
+   const user = users.find((user) => user.username.toLowerCase() === username.toLowerCase());
+
+   if (!user) return [];
+
+   const userFavourites = getFavouritesByUsername.filter((favourites) => favourites.user.user_id === user.user_id);
+
+   return userFavourites;
+}
+
+// export const postFavourite = ({ userId, itineraryId }) => {
+//    const user = users.find((user) => user.user_id === userId);
+//    const itinerary = itineraries.find((itinerary) => itinerary.itinerary_id === itineraryId);
+
+//    if (!user || !itinerary) {
+//       throw new Error("User or Itinerary not found");
+//    }
+
+//    const newFavourite = {
+
+//    }
+// }
+
+
+
+
