b46e2f99d7122e80449fa1219506b7c1f44addf9
Liam Appleton <liamappleton97@outlook.com>
2025-04-21 15:19:09 +0100

    adding form input to conditionally render with editClicked

--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,23 +1,42 @@
-import { useContext } from 'react';
-import { MenuPopover } from './MenuPopover';
+import { useContext, useState } from 'react';
+import Form from 'react-bootstrap/Form';
 import { UserContext } from '../context/User';
 import { MdEdit } from 'react-icons/md';
+import { FaCheckCircle } from 'react-icons/fa';
 
 export const Note = ({ text, userId }) => {
   const { loggedInUser } = useContext(UserContext);
+  const [editClicked, setEditClicked] = useState(false);
 
-  const handleClick = () => {};
+  const handleClick = (e) => {
+    e.preventDefault();
+    setEditClicked(!editClicked);
+  };
 
   return (
     <div className="d-flex gap-2 justify-content-center align-items-center">
-      <p className="mb-0">{text}</p>
-      {loggedInUser.user_id === userId && (
-        <MdEdit
-          className="react-icon menu-options p-0"
-          onClick={handleClick}
-          size={15}
-        />
+      {editClicked ? (
+        <Form>
+          <Form.Control placeholder={text}></Form.Control>
+        </Form>
+      ) : (
+        <p className="mb-0">{text}</p>
       )}
+
+      {loggedInUser.user_id === userId &&
+        (editClicked ? (
+          <FaCheckCircle
+            className="react-icon menu-options p-0"
+            onClick={handleClick}
+            size={15}
+          />
+        ) : (
+          <MdEdit
+            className="react-icon menu-options p-0"
+            onClick={handleClick}
+            size={15}
+          />
+        ))}
     </div>
   );
 };
