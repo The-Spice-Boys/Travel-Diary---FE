bbdf4e499cdb1269e49421e7bf1977d61a5f1e70
Liam Appleton <liamappleton97@outlook.com>
2025-04-21 10:37:36 +0100

    fixed bug in Favourites component

---
+++ src/components/MenuOptions.jsx
@@ -0,0 +1,14 @@
+import { FaEdit } from 'react-icons/fa';
+import { MdDelete } from 'react-icons/md';
+
+export const MenuOptions = () => {
+  const handleEditClick = () => {};
+  const handleDeleteClick = () => {};
+
+  return (
+    <div>
+      <FaEdit />
+      <MdDelete />
+    </div>
+  );
+};
