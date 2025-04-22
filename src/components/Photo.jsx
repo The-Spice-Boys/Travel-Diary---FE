0d2e4de16a3a38bf432b94bdf5ebbf9b7a058eec
Liam Appleton <liamappleton97@outlook.com>
2025-04-22 16:15:57 +0100

    adding comment to photo

--- src/components/Photo.jsx
+++ src/components/Photo.jsx
@@ -1,11 +1,11 @@
-import { Figure } from "react-bootstrap";
-import { MenuPopover } from "./MenuPopover";
-import { useContext } from "react";
-import { UserContext } from "../context/User";
+import { Figure } from 'react-bootstrap';
+import { MenuPopover } from './MenuPopover';
+import { useContext } from 'react';
+import { UserContext } from '../context/User';
 
 export const Photo = ({ url, caption, userId }) => {
   const { loggedInUser } = useContext(UserContext);
-
+  //! need photoId to delete
   return (
     <Figure className="position-relative d-inline-block">
       <Figure.Image src={url} className="rounded" />
