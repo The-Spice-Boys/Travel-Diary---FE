74a18c40b01e84e31f116697687713af691485b8
Liam Appleton <liamappleton97@outlook.com>
2025-04-24 12:32:20 +0100

    adding styling

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -1,12 +1,15 @@
-export const Error = ({ error }) => {
-   const lookupMsg = {
-      "404": "Not found",
-      "400": "Bad request",
-   };
+import { TbFileSad } from 'react-icons/tb';
 
-   return (
-      <div>
-         <p>Error {error}: {lookupMsg[error]}</p>
-      </div>
-   );
-}
+export const Error = ({ error }) => {
+  const lookupMsg = {
+    404: 'No itineraries have been created for this country yet!',
+    400: 'Bad request',
+  };
+
+  return (
+    <div className="width-card d-flex align-items-center justify-content-center mt-5">
+      <TbFileSad size={30} />
+      <p className="p-0 m-0">{lookupMsg[error]}</p>
+    </div>
+  );
+};
