2f54675b941bc3482515a185ceee53cc35f522d3
Liam Appleton <liamappleton97@outlook.com>
2025-04-24 15:35:59 +0100

    adjusting spinner styling

--- src/components/Loading.jsx
+++ src/components/Loading.jsx
@@ -1,9 +1,9 @@
-import { Spinner } from "react-bootstrap";
+import { Spinner } from 'react-bootstrap';
 
 export const Loading = () => {
   return (
-    <div className="d-flex justify-content-center align-items-center mt-3">
-      <Spinner variant="primary" />
+    <div className="d-flex justify-content-center align-items-center mt-5">
+      <Spinner style={{ color: '#b91c1cb3' }} />
     </div>
   );
 };
